b2ab98fe3b99cbf53d229d45cac768379e39b6d0
CkopnuoH91 <ckopnuoh.victor@yandex.by>
2018-01-14 21:11:59 +0300

    fix task after review

--- Tasks/Task1/Task1.js
+++ Tasks/Task1/Task1.js
@@ -1,123 +1,121 @@
-﻿function Add(a, b) {
+﻿function add(a, b) {
     return a + b;
 }
 
-function Sub(a, b) {
+function sub(a, b) {
     return a - b;
 }
 
-function Mul(a, b) {
+function mul(a, b) {
     return a * b;
 }
 
-function Div(a, b) {
-    if (b != 0) {
-        return a / b;
-    } else {
-        document.write("Divide by zero!");
-    }
+function div(a, b) {
+    return a / b;
 }
 
-function IntegerDivision(a, b) {
-    if (b != 0) {
-        return (a / b) >> 0;
-    } else {
-        document.write("Divide by zero!");
-    }
+function integerDivision(a, b) {
+    return (a / b) >> 0;
 }
 
-function DivisionWithRemainder(a, b) {
-    if (b != 0) {
-        return a % b;
-    } else {
-        document.write("Divide by zero!");
-    }
+function divisionWithRemainder(a, b) {
+    return a % b;
 }
 
-document.write("<hr/>Число_объект<hr/>");
-document.write("Add(2, {}): " + Add(2, {}) + "<br/>");
-document.write("Sub(2, {}): " + Sub(2, {}) + "<br/>");
-document.write("Mul(2, {}): " + Mul(2, {}) + "<br/>");
-document.write("Div(2, {}): " + Div(2, {}) + "<br/>");
-document.write("IntegerDivision(2, {}): " + IntegerDivision(2, {}) + "<br/>");
-document.write("DivisionWithRemainder(2, {}): " + DivisionWithRemainder(2, {}) + "<br/>");
+console.log("Number_Object");
+console.log("add(2, {}): " + add(2, {}));
+console.log("sub(2, {}): " + sub(2, {}));
+console.log("mul(2, {}): " + mul(2, {}));
+console.log("div(2, {}): " + div(2, {}));
+console.log("integerDivision(2, {}): " + integerDivision(2, {}));
+console.log("divisionWithRemainder(2, {}): " + divisionWithRemainder(2, {}));
+console.log("\n");
 
-document.write("<hr/>Число_строка<hr/>");
-document.write("Add(2, 'dfdsf'): " + Add(2, "dfdsf") + "<br/>");
-document.write("Sub(2, 'dfdsf'): " + Sub(2, "dfdsf") + "<br/>");
-document.write("Mul(2, 'dfdsf'): " + Mul(2, "dfdsf") + "<br/>");
-document.write("Div(2, 'dfdsf'): " + Div(2, "dfdsf") + "<br/>");
-document.write("IntegerDivision(2, 'dfdsf'): " + IntegerDivision(2, "dfdsf") + "<br/>");
-document.write("DivisionWithRemainder(2, 'dfdsf'): " + DivisionWithRemainder(2, "dfdsf") + "<br/>");
+console.log("Number_String");
+console.log("add(2, 'dfdsf'): " + add(2, "dfdsf"));
+console.log("sub(2, 'dfdsf'): " + sub(2, "dfdsf"));
+console.log("mul(2, 'dfdsf'): " + mul(2, "dfdsf"));
+console.log("div(2, 'dfdsf'): " + div(2, "dfdsf"));
+console.log("integerDivision(2, 'dfdsf'): " + integerDivision(2, "dfdsf"));
+console.log("divisionWithRemainder(2, 'dfdsf'): " + divisionWithRemainder(2, "dfdsf"));
+console.log("\n");
 
-document.write("<hr/>Число_массив<hr/>");
-document.write("Add(2, []): " + Add(2, []) + "<br/>");
-document.write("Sub(2, []): " + Sub(2, []) + "<br/>");
-document.write("Mul(2, []): " + Mul(2, []) + "<br/>");
-document.write("Div(2, []): " + Div(2, []) + "<br/>");
-document.write("IntegerDivision(2, []): " + IntegerDivision(2, []) + "<br/>");
-document.write("DivisionWithRemainder(2, []): " + DivisionWithRemainder(2, []) + "<br/>");
+console.log("Number_Array");
+console.log("add(2, []): " + add(2, []));
+console.log("sub(2, []): " + sub(2, []));
+console.log("mul(2, []): " + mul(2, []));
+console.log("div(2, []): " + div(2, []));
+console.log("integerDivision(2, []): " + integerDivision(2, []));
+console.log("divisionWithRemainder(2, []): " + divisionWithRemainder(2, []));
+console.log("\n");
 
-document.write("<hr/>Число_boolean<hr/>");
-document.write("Add(2, true): " + Add(2, true) + "<br/>");
-document.write("Sub(2, true): " + Sub(2, true) + "<br/>");
-document.write("Mul(2, true): " + Mul(2, true) + "<br/>");
-document.write("Div(2, true): " + Div(2, true) + "<br/>");
-document.write("IntegerDivision(2, true): " + IntegerDivision(2, true) + "<br/>");
-document.write("DivisionWithRemainder(2, true): " + DivisionWithRemainder(2, true) + "<br/>");
+console.log("Number_Boolean");
+console.log("add(2, true): " + add(2, true));
+console.log("sub(2, true): " + sub(2, true));
+console.log("mul(2, true): " + mul(2, true));
+console.log("div(2, true): " + div(2, true));
+console.log("integerDivision(2, true): " + integerDivision(2, true));
+console.log("divisionWithRemainder(2, true): " + divisionWithRemainder(2, true));
+console.log("\n");
 
-document.write("<hr/>Число_boolean<hr/>");
-document.write("Add(2, false): " + Add(2, false) + "<br/>");
-document.write("Sub(2, false): " + Sub(2, false) + "<br/>");
-document.write("Mul(2, false): " + Mul(2, false) + "<br/>");
-document.write("Div(2, false): " + Div(2, false) + "<br/>");
-document.write("IntegerDivision(2, false): " + IntegerDivision(2, false) + "<br/>");
-document.write("DivisionWithRemainder(2, false): " + DivisionWithRemainder(2, false) + "<br/>");
+console.log("Number_Boolean");
+console.log("add(2, false): " + add(2, false));
+console.log("sub(2, false): " + sub(2, false));
+console.log("mul(2, false): " + mul(2, false));
+console.log("div(2, false): " + div(2, false));
+console.log("integerDivision(2, false): " + integerDivision(2, false));
+console.log("divisionWithRemainder(2, false): " + divisionWithRemainder(2, false));
+console.log("\n");
 
-document.write("<hr/>Число_число<hr/>");
-document.write("Add(7, 2): " + Add(7, 2) + "<br/>");
-document.write("Sub(7, 2): " + Sub(7, 2) + "<br/>");
-document.write("Mul(7, 2): " + Mul(7, 2) + "<br/>");
-document.write("Div(7, 2): " + Div(7, 2) + "<br/>");
-document.write("IntegerDivision(7, 2): " + IntegerDivision(7, 2) + "<br/>");
-document.write("DivisionWithRemainder(7, 2): " + DivisionWithRemainder(7, 2) + "<br/>");
+console.log("Number_Number");
+console.log("add(7, 2): " + add(7, 2));
+console.log("sub(7, 2): " + sub(7, 2));
+console.log("mul(7, 2): " + mul(7, 2));
+console.log("div(7, 2): " + div(7, 2));
+console.log("integerDivision(7, 2): " + integerDivision(7, 2));
+console.log("divisionWithRemainder(7, 2): " + divisionWithRemainder(7, 2));
+console.log("\n");
 
-document.write("<hr/>Массив_массив<hr/>");
-document.write("Add([], []): " + Add([], []) + "<br/>");
-document.write("Sub([], []): " + Sub([], []) + "<br/>");
-document.write("Mul([], []): " + Mul([], []) + "<br/>");
-document.write("Div([], []): " + Div([], []) + "<br/>");
-document.write("IntegerDivision([], []): " + IntegerDivision([], []) + "<br/>");
-document.write("DivisionWithRemainder([], []): " + DivisionWithRemainder([], []) + "<br/>");
+console.log("Array_Array");
+console.log("add([], []): " + add([], []));
+console.log("sub([], []): " + sub([], []));
+console.log("mul([], []): " + mul([], []));
+console.log("div([], []): " + div([], []));
+console.log("integerDivision([], []): " + integerDivision([], []));
+console.log("divisionWithRemainder([], []): " + divisionWithRemainder([], []));
+console.log("\n");
 
-document.write("<hr/>Массив_массив<hr/>");
-document.write("Add([1,2,3], [4, 5, 6]): " + Add([1, 2, 3], [4, 5, 6]) + "<br/>");
-document.write("Sub([1,2,3], [4, 5, 6]): " + Sub([1, 2, 3], [4, 5, 6]) + "<br/>");
-document.write("Mul([1,2,3], [4, 5, 6]): " + Mul([1, 2, 3], [4, 5, 6]) + "<br/>");
-document.write("Div([1,2,3], [4, 5, 6]): " + Div([1, 2, 3], [4, 5, 6]) + "<br/>");
-document.write("IntegerDivision([1,2,3], [4, 5, 6]): " + IntegerDivision([1, 2, 3], [4, 5, 6]) + "<br/>");
-document.write("DivisionWithRemainder([1,2,3], [4, 5, 6]): " + DivisionWithRemainder([1, 2, 3], [4, 5, 6]) + "<br/>");
+console.log("Array_Array");
+console.log("add([1,2,3], [4, 5, 6]): " + add([1, 2, 3], [4, 5, 6]));
+console.log("sub([1,2,3], [4, 5, 6]): " + sub([1, 2, 3], [4, 5, 6]));
+console.log("mul([1,2,3], [4, 5, 6]): " + mul([1, 2, 3], [4, 5, 6]));
+console.log("div([1,2,3], [4, 5, 6]): " + div([1, 2, 3], [4, 5, 6]));
+console.log("integerDivision([1,2,3], [4, 5, 6]): " + integerDivision([1, 2, 3], [4, 5, 6]));
+console.log("divisionWithRemainder([1,2,3], [4, 5, 6]): " + divisionWithRemainder([1, 2, 3], [4, 5, 6]));
+console.log("\n");
 
-document.write("<hr/>boolean_boolean<hr/>");
-document.write("Add(false, false): " + Add(false, false) + "<br/>");
-document.write("Sub(false, false): " + Sub(false, false) + "<br/>");
-document.write("Mul(false, false): " + Mul(false, false) + "<br/>");
-document.write("Div(false, false): " + Div(false, false) + "<br/>");
-document.write("IntegerDivision(false, false): " + IntegerDivision(false, false) + "<br/>");
-document.write("DivisionWithRemainder(false, false): " + DivisionWithRemainder(false, false) + "<br/>");
+console.log("Boolean_Boolean");
+console.log("add(false, false): " + add(false, false));
+console.log("sub(false, false): " + sub(false, false));
+console.log("mul(false, false): " + mul(false, false));
+console.log("div(false, false): " + div(false, false));
+console.log("integerDivision(false, false): " + integerDivision(false, false));
+console.log("divisionWithRemainder(false, false): " + divisionWithRemainder(false, false));
+console.log("\n");
 
-document.write("<hr/>Объект_Объект<hr/>");
-document.write("Add({}, {}): " + Add({}, {}) + "<br/>");
-document.write("Sub({}, {}): " + Sub({}, {}) + "<br/>");
-document.write("Mul({}, {}): " + Mul({}, {}) + "<br/>");
-document.write("Div({}, {}): " + Div({}, {}) + "<br/>");
-document.write("IntegerDivision({}, {}): " + IntegerDivision({}, {}) + "<br/>");
-document.write("DivisionWithRemainder({}, {}): " + DivisionWithRemainder({}, {}) + "<br/>");
+console.log("Object_Object");
+console.log("add({}, {}): " + add({}, {}));
+console.log("sub({}, {}): " + sub({}, {}));
+console.log("mul({}, {}): " + mul({}, {}));
+console.log("div({}, {}): " + div({}, {}));
+console.log("integerDivision({}, {}): " + integerDivision({}, {}));
+console.log("divisionWithRemainder({}, {}): " + divisionWithRemainder({}, {}));
+console.log("\n");
 
-document.write("<hr/>Объект_массив<hr/>");
-document.write("Add({}, []): " + Add({}, []) + "<br/>");
-document.write("Sub({}, []): " + Sub({}, []) + "<br/>");
-document.write("Mul({}, []): " + Mul({}, []) + "<br/>");
-document.write("Div({}, []): " + Div({}, []) + "<br/>");
-document.write("IntegerDivision({}, []): " + IntegerDivision({}, []) + "<br/>");
-document.write("DivisionWithRemainder({}, []): " + DivisionWithRemainder({}, []) + "<br/>");
+console.log("Object_Array");
+console.log("add({}, []): " + add({}, []));
+console.log("sub({}, []): " + sub({}, []));
+console.log("mul({}, []): " + mul({}, []));
+console.log("div({}, []): " + div({}, []));
+console.log("integerDivision({}, []): " + integerDivision({}, []));
+console.log("divisionWithRemainder({}, []): " + divisionWithRemainder({}, []));
